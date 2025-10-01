395e807b2100cef2e77124b27db5417c0d5301c6
raffiabyann <raffizizou4@gmail.com>
2025-10-01 21:26:03 +0700

    Second Commit

---
+++ src/week06/raffi/id/umn/ac/Handphone.java
@@ -0,0 +1,15 @@
+package week06.raffi.id.umn.ac;
+
+public class Handphone extends Barang {
+    private String warna;
+    public static int total = 0;
+
+    public Handphone(int id, String nama, double harga, int stok, String warna) {
+    	super(id, harga, nama, stok);
+        this.warna = warna;
+        total++;
+    }
+
+    public String getWarna() { 
+    	return warna; }
+}
